0a2312c5bd71093d97aa66aeffee3afa56d4f5dc
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2017-12-03 20:19:50 +0100

    Add historicalExchangeRates as reducer for fetching historical currency rates

---
+++ src/state/historicalExchangeRates.js
@@ -0,0 +1,49 @@
+const BEGIN = 'GET_BEGIN'
+const SUCCESS = 'GET_SUCCESS'
+const FAIL = 'GET_FAIL'
+
+export const getHistoricalCurrencies = (startDate, endDate) => dispatch => {
+  dispatch({ type: BEGIN })
+  fetch(
+    'http://api.nbp.pl/api/exchangerates/tables/A?format=json'
+  ).then(
+    response => response.json()
+  ).then(
+    data => dispatch({ type: SUCCESS, data: data[0].rates })
+  ).catch(
+    error => dispatch({ type: FAIL, error })
+  )
+}
+
+const initialState = {
+  historicalData: [],
+  getting: false,
+  adding: false,
+  removing: false,
+  error: null
+}
+
+export default (state = initialState, action = {}) => {
+  switch(action.type) {
+    case BEGIN:
+      return {
+        ...state,
+        getting: true,
+        error: null
+      }
+    case SUCCESS:
+      return {
+        ...state,
+        data: action.data,
+        getting: false
+      }
+    case FAIL:
+      return {
+        ...state,
+        getting: false,
+        error: action.error
+      }
+    default:
+      return state
+  }
+}

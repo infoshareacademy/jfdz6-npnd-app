6da23a7d78f7a279cad706869090ce254ee2fafc
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2017-12-03 12:52:22 +0100

    Show rates for selected currency

--- src/CurrencyRates.js
+++ src/CurrencyRates.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { FormGroup, Label, Input } from 'reactstrap'
 
 /*
 Aplikacja powinna umożliwić sprawdzenie aktualnego  CURRENT_RATE_Currency(?)
@@ -18,9 +19,16 @@ Wybrane kursy można pobierać na bieżąco z API NBP.
 class CurrencyRates extends React.Component {
 
   state = {
-    rates: []
+    rates: [],
+    selectedRate: ''
   }
 
+  handleChange = event => {
+    console.log(event.target.value)
+    this.setState( {
+      selectedRate: event.target.value
+    })
+  }
 
   componentDidMount() {
     fetch('http://api.nbp.pl/api/exchangerates/tables/A?format=json')
@@ -33,9 +41,17 @@ class CurrencyRates extends React.Component {
     return (
       <div>
         Currency Rates
-        <ul>
-        {this.state.rates.map(rate => <li>{rate.currency} {rate.code} {rate.mid}</li>)}
-        </ul>
+        <FormGroup>
+          <Label for="exampleSelect">Choose calculator </Label>
+          <Input type="select" name="select" id="exampleSelect" onChange={this.handleChange} >
+            {this.state.rates.map(rate => <option>{rate.currency}</option>)}
+          </Input>
+        </FormGroup>
+
+
+        {this.state.rates.filter(rate => rate.currency === this.state.selectedRate ).map( e =><p> {e.currency}  {e.mid}</p>)}
+
+
       </div>
 
     )

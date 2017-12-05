09412caac3405661fa516c2ff21d5abc696fe19c
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2017-12-05 20:34:25 +0100

    Hide unnecessary content for better view of work

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <CurrencyRates/>
-        <Calculator />
+        {/*<Calculator />*/}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>

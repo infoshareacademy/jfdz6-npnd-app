d2fc833fd490897d2f38c0bf312f9628b8032069
piotrbaranski <p.baranski@yahoo.pl>
2017-12-12 08:04:39 +0100

    Add dropdown button from reactstrap. Code refactor.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 import {getCurrencies} from "./state/exchangeRates"
 
 import CalculatorInputCurrency from './CalculatorInputCurrency'
 import CalculatorOutputCurrency from './CalculatorOutputCurrency'
+import Example from './DropdownButton'
 
 class Calculator extends Component {
 
   componentDidMount() {
     this.props.getCurrencies()
   }
+
   render() {
     return (
       <div>
-      <CalculatorInputCurrency />
-        {this.props.rates.map(rate => <li>{rate.currency}  {rate.mid}</li>)}
-      <CalculatorOutputCurrency />
+        <CalculatorInputCurrency/>
+        {/*{this.props.rates.map(rate => <li>{rate.currency}  {rate.mid}</li>)}*/}
+        <CalculatorOutputCurrency/>
+        <Example/>
       </div>
     )
   }

4d9afbb78ed9d3b18943c533fa90a346d2d57b50
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2017-12-12 09:50:35 +0100

    Prepare Wallet for later development

--- src/Wallet.js
+++ src/Wallet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getCurrencies } from "./state/exchangeRates"
-import { FormGroup, Label, Input, Button, Table } from 'reactstrap'
+import { Table } from 'reactstrap'
 
 
 /*
@@ -17,36 +17,16 @@ użytkownik powinien zobaczyć komunikat o potencjalnej korzyści ze sprzedaży.
 
 class Wallet extends React.Component {
 
-  state = {
-    selectedRate: ''
-  }
 
-  handleChange = event => {
-
-    this.setState({
-      selectedRate: event.target.value
-    })
-  }
 
   render() {
-    return(
+    return (
       <div>
         <h1>My Wallet</h1>
-        <FormGroup>
-          <Label for="exampleSelect">Choose currency </Label>
-          <Input type="select" name="select" id="exampleSelect" placeholder="-" onChange={this.handleChange}>
-            {this.props.rates.map(rate => <option>{rate.currency}</option>)}
-          </Input>
-        </FormGroup>
 
-        {
-          this.props.rates.filter(rate => rate.currency === this.state.selectedRate)
-            .map(e => <p> {e.currency} {e.mid}</p>)
-        }
-        <Table hover>
+        <Table hover size="sm" responsive>
           <thead>
           <tr>
-            <th>#</th>
             <th>Currency</th>
             <th>Today rates</th>
             <th>Date of buy</th>
@@ -56,33 +36,18 @@ class Wallet extends React.Component {
           </tr>
           </thead>
           <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-          </tr>
+          {this.props.rates.map(
+            rate => <tr
+            key={rate.code}
+            >
+              {rate.currency}
+              <td>
+                {
+                  this.props.rates.filter(rate2 => rate2.code === rate.code)
+                    .map(e => <span key={e.code}> {e.mid}</span>)
+                }
+              </td>
+            </tr>)}
           </tbody>
         </Table>
 
@@ -102,4 +67,4 @@ const mapDispatchToProps = dispatch => ({
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps) (Wallet)
+  mapDispatchToProps)(Wallet)

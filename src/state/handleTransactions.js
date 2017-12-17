fc08730688a78f468bdbc7d80568fc0a77716a7f
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2017-12-17 13:19:27 +0100

    Fix passing data to reducer

--- src/state/handleTransactions.js
+++ src/state/handleTransactions.js
@@ -1,21 +1,21 @@
 const BUY = 'handleTransaction/BUY'
 
-export const buyCurrency = (transactionId,currencyCode,currencyAmount,transactionRate,dateOfTransaction) => dispatch => {
-  dispatch({ type: BUY, transactionData: currencyCode  })
+export const buyCurrency = (transactionData) => dispatch => {
+  dispatch({ type: BUY, transactionData  })
 }
 
 const initialState = {
-  budget: 10000
+  budget: 10000,
+  transactions: []
 }
 
 
 export default ( state = initialState, action = {}) => {
   switch (action.type) {
     case BUY: {
-      const { type, ...transaction } = action
       return {
         ...state,
-         transaction
+         transactions: state.transactions.concat(action.transactionData)
       }
     }
 

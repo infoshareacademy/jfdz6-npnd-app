0e773077dc1ec62804af6fa7dfcd6428a284ed80
piotrbaranski <p.baranski@yahoo.pl>
2018-01-07 09:14:28 +0100

    Install Octicon package. Add icon instead of word to sign out button on small devices.

--- src/SignOut.js
+++ src/SignOut.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Media from 'react-media'
-import {connect} from 'react-redux'
-import {Button} from 'reactstrap'
+import { connect } from 'react-redux'
+import { Button } from 'reactstrap'
+import Octicon from 'react-octicon'
 
 import {signOut} from './state/auth'
 import './SignOut.css'
@@ -16,7 +17,8 @@ class SignOut extends Component {
     return (
       <Media query="(max-width: 576px)">
         {matches => matches ? (
-          <Button id='signOutButton' type='submit' color='primary' size='sm' onClick={this.handleSignOut}>&#xe389</Button>) : (
+          <Button id='signOutButton' type='submit' color='primary' size='md'
+                  onClick={this.handleSignOut}><Octicon name='sign-out'/></Button>) : (
           <Button id='signOutButton' type='submit' color='primary' size='lg' onClick={this.handleSignOut}>Wyloguj
             się</Button>)}
       </Media>

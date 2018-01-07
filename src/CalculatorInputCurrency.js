b2bef995a64c7918588b08e0c6fe950825adc984
piotrbaranski <p.baranski@yahoo.pl>
2018-01-07 18:04:09 +0100

    Remove unnecessary components. Include all form group components in one file.

--- src/CalculatorInputCurrency.js
+++ src/CalculatorInputCurrency.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Form, Input, Label } from 'reactstrap'
-import CurrencyList from './CurrencyList'
+import { Button, FormGroup, Form, Input, Label } from 'reactstrap'
 
-import {add} from './state/exchangeRates'
-import OutputCurrencyList from './OutputCurrencyList'
+import { add, changeCurrency, changeOutputCurrency } from './state/exchangeRates'
 import './CalculatorInputCurrency.css'
 
 class CalculatorInputCurrency extends Component {
@@ -17,28 +15,65 @@ class CalculatorInputCurrency extends Component {
     this.userInput.value = ''
   }
 
+  handleSelect = event => {
+    const optionField = event.target.value
+    this.props.selectCurrency(optionField)
+  }
+
+  handleOutputSelect = event => {
+    const optionField = event.target.value
+    this.props.selectOutputCurrency(optionField)
+  }
+
   render() {
     return (
       <div>
         <Form onSubmit={this.handleSubmit}>
-          <div className='flex-container'>
-            <div className='input-field'>
-              <Label for="input">Wpisz kwotę</Label>
-              <Input id='input'
-                     type='text'
-                     ref={item => this.userInput = item}
-                     placeholder={this.props.error ? this.props.error.message : this.props.userValue} required/>
-            </div>
-            <div className='currency-list'>
-              <CurrencyList/>
-            </div>
-            <div className='currency-list'>
-              <OutputCurrencyList/>
-            </div>
-            <div className='submit-button'>
-              <Button type="submit" bsSize="lg">Przelicz</Button>
-            </div>
-          </div>
+          <FormGroup>
+            <Label for="input">Wpisz kwotę</Label>
+            <Input id='input'
+                   type='text'
+                   ref={item => this.userInput = item}
+                   placeholder={this.props.error ? this.props.error.message : this.props.userValue}
+                   required/>
+          </FormGroup>
+          <FormGroup>
+            <Label for="inputCurrency">Wybierz walutę, z której chcesz przeliczyć</Label>
+            <Input type="select"
+                   name="select"
+                   id="inputCurrency"
+                   value={this.props.selectValue}
+                   onChange={this.handleSelect}>
+              {
+                this.props.rates.map(
+                  rate => <option
+                    value={rate.code}
+                    key={rate.code}>
+                    {rate.currency}
+                  </option>
+                )
+              }
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="outputCurrency">Wybierz walutę, na którą chcesz przeliczyć</Label>
+            <Input type="select"
+                   name="select"
+                   id="outputCurrency"
+                   value={this.props.selectOutputValue}
+                   onChange={this.handleOutputSelect}>
+              {
+                this.props.rates.map(
+                  rate => <option
+                    value={rate.code}
+                    key={rate.code}>
+                    {rate.currency}
+                  </option>
+                )
+              }
+            </Input>
+          </FormGroup>
+          <Button type="submit" bsSize="lg">Przelicz</Button>
         </Form>
       </div>
     )
@@ -48,11 +83,15 @@ class CalculatorInputCurrency extends Component {
 const mapStateToProps = state => ({
   rates: state.exchangeRates.data,
   error: state.exchangeRates.error,
-  userValue: state.exchangeRates.userValue
+  userValue: state.exchangeRates.userValue,
+  selectValue: state.exchangeRates.selectValue,
+  selectOutputValue: state.exchangeRates.selectOutputValue
 })
 
 const mapDispatchToProps = dispatch => ({
   addValue: (userValue) => dispatch(add(userValue)),
+  selectCurrency: (selectValue) => dispatch(changeCurrency(selectValue)),
+  selectOutputCurrency: (selectOutputValue) => dispatch(changeOutputCurrency(selectOutputValue))
 })
 
 export default connect(

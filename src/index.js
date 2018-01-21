62a3c54f5fbcfcad096c24c5b77f717d91edfe87
Natalia Demytruk <ndemytruk@gmail.com>
2018-01-21 13:19:40 +0100

    Change order of style imports

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
+import './index.css';
 import Auth from './Auth'
 
 // Aplikacja powinna umożliwić szybkie przeliczenie dowolnej ilości wybranej waluty na inną.

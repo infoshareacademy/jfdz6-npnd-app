b223e2b3c17f446a4df721c113b913f41dda8f45
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2018-01-23 15:51:28 +0100

    Display all transactions log-like

--- src/Budget.js
+++ src/Budget.js
@@ -1,15 +1,68 @@
 import React from 'react'
-import { connect } from 'react-router'
-import getTransactions from './utils'
+import { connect } from 'react-redux'
+import { Table } from 'reactstrap'
 
 class Budget extends React.Component {
-  render () {
+
+
+  render() {
     return (
       <div>
-        Budget
+        <h2>Logi transkacji</h2>
+        <Table hover size="sm" responsive>
+          <thead>
+          <tr>
+            <th>Waluta</th>
+            <th>Kurs transakcji</th>
+            <th>Ilość</th>
+            <th>Data transakcji</th>
+          </tr>
+          </thead>
+          <tbody>
+          {
+            (this.props.transactions).map(
+              rate => <tr
+                key={rate.transactionId}
+                data-item-id={rate.currencyCode}
+                data-item-amount={rate.currencyAmount}
+                data-item-rate={rate.transactionRate}
+              >
+                {rate.currencyCode}
+                <td>
+                  {
+                    this.props.transactions.filter(rate2 => rate2.transactionId === rate.transactionId)
+                      .map(e => <span key={e.transactionId}> {e.transactionRate}</span>)
+                  }
+                </td>
+                <td>
+                  {
+                    this.props.transactions.filter(rate2 => rate2.transactionId === rate.transactionId)
+                      .map(e => <span key={e.transactionId}> {e.currencyAmount}</span>)
+                  }
+                </td>
+                <td>
+                  {
+                    this.props.transactions.filter(rate2 => rate2.transactionId === rate.transactionId)
+                      .map(e => <span key={e.transactionId}> {e.dateOfTransaction}</span>)
+                  }
+                </td>
+              </tr>)}
+          </tbody>
+        </Table>
+
+
       </div>
-            )
+    )
   }
 }
 
-export default connect()(Budget)
+const mapStateToProps = state => ({
+  rates: state.exchangeRates.data,
+  transactions: state.handleTransactions.transactions,
+  auth: state.auth
+})
+
+
+export default connect(
+  mapStateToProps,
+)(Budget)

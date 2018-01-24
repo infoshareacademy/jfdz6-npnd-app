a71ea2710227f9e53d97103e5c01f434f5580ca0
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2018-01-24 02:42:33 +0100

    Add random color generator function to utils.

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export default function getTransactions (transactions) {
+export function getTransactions (transactions) {
   return transactions.reduce((result, next) => {
     result.filter(e => e.transactionKey === next.transactionKey).length > 0 ?
       result[result.findIndex(item => item.transactionKey === next.transactionKey )].currencyAmount = result[result.findIndex(item => item.transactionKey === next.transactionKey)].currencyAmount + next.currencyAmount
@@ -11,4 +11,13 @@ export default function getTransactions (transactions) {
       })
     return result
   }, [])
+}
+
+export function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
